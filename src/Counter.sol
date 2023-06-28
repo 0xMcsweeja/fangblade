77714b78b80bb425a1db0ccf45aa32ed6962224a
0xMcsweeja <0xmcsweeja@gmail.com>
2023-06-29 07:21:07 +1000

    optimizoooor

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,39 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Counter {
-    uint256 public number;
+    uint256 private _count; // [Storage - at default location for state variable] This is implicitly at location 0x00 for a single variable contract
 
+    // Function to set the value of the counter
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        _count = newNumber; // [Storage] Set the storage counter to the provided value
     }
 
+    // Function to increment the counter by 1
     function increment() public {
-        number++;
+        _count += 1; // [Storage] Increment the storage counter
     }
+
+    // Optimized function to increment the counter using inline assembly
+    function incrementOptimized() public {
+        assembly {
+            let count := sload(0)  // [Storage - at location 0x00] Load the value at storage slot 0 into a memory variable
+            let incremented := add(count, 1) // [Memory] Increment the memory variable by 1
+            sstore(0x00, incremented) // [Storage - at location 0x00] Store the incremented memory value back at storage slot 0
+        }
+    }
+    
+    // Function to get the value of the counter
+    function getCount() public view returns (uint256) {
+        return _count; // [Storage] Return the value of the storage counter
+    }
+
+    // Optimized function to get the counter's value using inline assembly
+    function getCountOptimized() public view returns (uint256 result) {
+        assembly {
+            result := sload(0) // [Storage - at location 0x00] Load the value at storage slot 0 into the memory result variable
+        }
+    }
+
+
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -13,12 +13,39 @@ contract CounterTest is Test {
     }
 
     function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        uint256 initialGas = gasleft(); // Get initial gas
+        for (uint256 i = 0; i < 100; i++) {
+            counter.increment();
+        }
+        uint256 finalGas = gasleft();
+        uint256 gasUsed = initialGas - finalGas;
+        console.log("Gas used in testIncrement:", gasUsed);
+        assertEq(counter.getCount(), 100);
+    }
+
+    function testIncrementOptimized() public {
+        uint256 initialGas = gasleft(); // Get initial gas
+        for (uint256 i = 0; i < 100; i++) {
+            counter.incrementOptimized();
+        }
+        uint256 finalGas = gasleft();
+        uint256 gasUsed = initialGas - finalGas;
+        console.log("Gas used in testIncrementOptimized:", gasUsed);
+        assertEq(counter.getCount(), 100);
+    }
+
+    function testSetNumberParameterizer() public pure returns (uint256) {
+        return 5; // This is the value that will be used for `x` in testSetNumber
     }
 
     function testSetNumber(uint256 x) public {
         counter.setNumber(x);
-        assertEq(counter.number(), x);
+        assertEq(counter.getCount(), x);
+    }
+
+    function testGetCountOptimized() public {
+        counter.increment(); // increments counter to 1
+        counter.incrementOptimized(); // increments counter to 2
+        assertEq(counter.getCountOptimized(), 2); // should return 2
     }
 }
