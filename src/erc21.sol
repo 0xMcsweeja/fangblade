5051d924e5073152bf3f2a7adfb167ca1af99162
0xMcsweeja <0xmcsweeja@gmail.com>
2023-07-02 08:59:08 +1000

    approve() done, allowance to come

--- src/erc21.sol
+++ src/erc21.sol
@@ -5,6 +5,15 @@ This project aims to implement an ERC20 token using Solidity assembly for effici
 The ERC21 token has the same interfaces as ERC20 but implemented with assembly code where possible. 
 This project serves as a learning tool to understand low-level Ethereum Virtual Machine (EVM) operations and their gas efficiency.
 
+- [x] `balanceOf`
+- [x] `transfer`
+- [x] `totalSupply`
+- [ ] `approve`
+- [ ] `allowance`
+- [ ] `transferFrom`
+- [ ] `increaseAllowance`
+- [ ] `decreaseAllowance`
+
 Storage Layout:
 -------------------
 |   Slot   |  Description  |
@@ -17,7 +26,8 @@ pragma solidity ^0.8.0;
 
 contract ERC21 {
     uint256 private constant _BALANCES_POSITION = 0x02;
-    uint256 private _totalSupply;
+    uint256 private constant _ALLOWANCES_POSITION = 0x03;
+    uint256 private _TOTAL_SUPPLY_POSITION;
 
     /*
     Assembly logic:
@@ -103,4 +113,46 @@ contract ERC21 {
         // Indicate that the transfer was successful
         return true;
     }   
+    /*  
+        1. Get the address of the token owner, i.e., the caller of this function.
+        2. Store the token owner's address, the spender's address, and the _ALLOWANCES_POSITION constant in suitable memory slots.
+        3. Hash the token owner's address, the spender's address, and the _ALLOWANCES_POSITION together to get the storage slot for the allowance.
+        4. Store the amount into the calculated allowance slot.
+    */
+    function approve(address spender, uint256 amount) public returns (bool) {
+        assembly {
+            // 1. You need the 'caller' function to get the address of the current function invoker (token holder)
+            // Store this address in a suitable memory slot.
+            let owner := caller()
+            // 2. Store the `spender` address and the _ALLOWANCES_POSITION constant in suitable memory slots.
+            mstore(0x00, owner) // caller address
+            mstore(0x20, spender)
+            mstore(0x40, _ALLOWANCES_POSITION)
+            // 3. Hash the token holder's address, the `spender` address, and _ALLOWANCES_POSITION together 
+            let slot := keccak256(0x00, 0x60)
+            // 4. Store the `amount` into the allowance slot obtained in the previous step.
+            sstore(slot, amount)
+            // 5. Emit the Approval event (You may skip this step as handling events in assembly is complex)
+        }
+        return true;
+    }
+
+     /*
+        Assembly logic:
+        1. Calculate the storage slot for the allowance of the `spender` for `owner`'s tokens.
+        2. This involves hashing the `owner`'s address, `spender`'s address, and the _ALLOWANCES_POSITION together.
+        3. Load the allowance from the computed slot.
+        4. Return the loaded value.
+    */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        uint256 remaining;
+        assembly {
+            // Store the `owner` address, `spender` address, and _ALLOWANCES_POSITION constant in suitable memory slots.
+            // Compute the storage slot by hashing these together.
+            // Load the allowance from the computed storage slot.
+        }
+        return remaining;
+    }
+
+
 }
